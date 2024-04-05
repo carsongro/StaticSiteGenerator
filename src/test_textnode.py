03b2d69ce8d03149dc4607ba61e5792d0fd8ff9a
Carson Gross <carsongro@gmail.com>
2024-04-05 16:38:49 -0600

    Added splitting by delimiter

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,12 +1,23 @@
 import unittest
 
-from textnode import TextNode
+from textnode import *
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node = TextNode("This is a text node", "bold")
         node2 = TextNode("This is a text node", "bold")
         self.assertEqual(node, node2)
+
+    def test_split_nodes_delimiter(self):
+        node = TextNode("This is text with a `code block` word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+        result = [
+            TextNode("This is text with a ", text_type_text),
+            TextNode("code block", text_type_code),
+            TextNode(" word", text_type_text),
+        ]
+        
+        self.assertEqual(new_nodes, result)
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -35,3 +35,21 @@ def text_node_to_html_node(text_node):
     elif type == text_type_image:
         return LeafNode("b", "", {"src": text_node.url, "alt": text_node.text})
     raise Exception(f"Could not find text type {type}")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if not node.text_type == text_type_text:
+            new_nodes.append(node)
+        if not node.text.count(delimiter) % 2 == 0:
+            raise Exception("The delimiter must be closed.")
+
+        components = node.text.split(delimiter)
+        for idx, component in enumerate(components):
+            if idx % 2 == 0:
+                new_nodes.append(TextNode(component, text_type_text))
+            else:
+                new_nodes.append(TextNode(component, text_type))
+
+    return new_nodes
+
