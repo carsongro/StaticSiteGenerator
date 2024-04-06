272ca4fe9d8c79ad952474ad7c1b24d2dfaa6104
Carson Gross <carsongro@gmail.com>
2024-04-06 13:45:07 -0600

    Implemented text to textnodes

--- src/textnode.py
+++ src/textnode.py
@@ -8,6 +8,10 @@ text_type_code = "code"
 text_type_link = "link"
 text_type_image = "image"
 
+bold_delimiter = "**"
+italic_delimiter = "*"
+code_delimiter = "`"
+
 class TextNode:
     def __init__(self, text, text_type, url=None) -> None:
         self.text = text
@@ -39,9 +43,11 @@ def text_node_to_html_node(text_node):
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
+
     for node in old_nodes:
         if not node.text_type == text_type_text:
             new_nodes.append(node)
+            continue
         if not node.text.count(delimiter) % 2 == 0:
             raise Exception("The delimiter must be closed.")
 
@@ -51,7 +57,6 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append(TextNode(component, text_type_text))
             else:
                 new_nodes.append(TextNode(component, text_type))
-
     return new_nodes
 
 
@@ -68,7 +73,7 @@ def split_nodes_image(old_nodes):
         images = extract_markdown_images(node.text)
 
         if len(images) == 0:
-            node = [TextNode(node.text, text_type_text)] if not (node.text == "" or node.text == "\n") else []
+            node = [node] if not (node.text == "" or node.text == "\n") else []
             return node
 
         components = node.text.split(f"![{images[0][0]}]({images[0][1]})", 1)
@@ -92,7 +97,7 @@ def split_nodes_link(old_nodes):
     def getNodeList(node):
         links = extract_markdown_links(node.text)
         if len(links) == 0:
-            node = [TextNode(node.text, text_type_text)] if not (node.text == "" or node.text == "\n") else []
+            node = [node] if not (node.text == "" or node.text == "\n") else []
             return node
 
         components = node.text.split(f"[{links[0][0]}]({links[0][1]})", 1)
@@ -109,3 +114,14 @@ def split_nodes_link(old_nodes):
         new_nodes.extend(getNodeList(node))
 
     return new_nodes
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+
+    nodes = split_nodes_delimiter(nodes, bold_delimiter, text_type_bold)
+    nodes = split_nodes_delimiter(nodes, italic_delimiter, text_type_italic)
+    nodes = split_nodes_delimiter(nodes, code_delimiter, text_type_code)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+
+    return nodes
