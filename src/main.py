ff28e65bfcc073bc7ba42be23a22861a21a4f689
Carson Gross <carsongro@gmail.com>
2024-04-06 20:44:07 -0600

    Added function to copy from one directory to another

--- src/main.py
+++ src/main.py
@@ -10,7 +10,22 @@ def copy_contents(from_directory, to_directory):
     shutil.rmtree(f"{to_directory}", True)
     os.mkdir("public")
 
-    print(os.listdir(from_directory))
+    def copy(from_directory, to_directory):
+        paths = os.listdir(from_directory)
+        for path in paths:
+            isFile = os.path.isfile(os.path.join(from_directory, path))
+            src, dst = os.path.join(from_directory, path), os.path.join(to_directory, path)
+            if isFile:
+                print(f"Copying: {src} to {dst}")
+                shutil.copy(src, dst)
+            else:
+                os.mkdir(dst)
+                copy(f"{src}", f"{dst}")
+    
+    copy(from_directory, to_directory)
+    
+
+
 
 if __name__ == '__main__':
     main()
