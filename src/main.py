447904f05d11ad957aaa06d5eb6177ff00b744c3
Carson Gross <carsongro@gmail.com>
2024-04-06 20:16:23 -0600

    Updated gitignore

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 from textnode import TextNode
+import shutil
+import os
 
 def main():
-    print(TextNode("This is a text node", "bold", "https://www.boot.dev"))
+    copy_contents("static", "public")
 
+def copy_contents(from_directory, to_directory):
+    # remove contents from old directory    
+    shutil.rmtree(f"{to_directory}", True)
+    os.mkdir("public")
+
+    print(os.listdir(from_directory))
 
 if __name__ == '__main__':
     main()
